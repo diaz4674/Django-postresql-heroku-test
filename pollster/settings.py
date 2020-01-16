06e059d82d65fcbe4170c37a74b09a87f84061af
Miguel <diaz4674@gmail.com>
2020-01-16 01:06:07 -0800

    updtes

--- pollster/settings.py
+++ pollster/settings.py
@@ -9,15 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+
 import os
-import environ
-import django_heroku
 
-env = environ.Env(
-    DEBUG=(bool,False)
-)
-
-environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = '2mh8hni0bwxsag(+^a*%)ei-5=+oh37(9(dd69m0ti96o+6%gw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,10 +78,10 @@ WSGI_APPLICATION = 'pollster.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST")
+        'NAME': 'pirates',
+        'USER': 'postgres',
+        'PASSWORD': 'Cc3058366',
+        'HOST': 'localhost'
     }
 }
 
@@ -129,4 +123,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
